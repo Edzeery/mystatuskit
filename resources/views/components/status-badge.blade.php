9bde7b5b88f6c7540885115a88a426c636290ef4
Edzeery <abdhafide98@gmail.com>
2026-07-13 23:26:20 +0100

    Update status-badge.blade.php

--- resources/views/components/status-badge.blade.php
+++ resources/views/components/status-badge.blade.php
@@ -1,3 +1,14 @@
+@props([
+    'domain',
+    'status',
+    'set'   => 'fa',
+    'class' => '',
+])
+
+@php
+    $result = \Edzeery\MyStatusKit\Facades\Status::for($domain, $status);
+@endphp
+
 <span {{ $attributes->merge(['class' => 'status-badge inline-flex items-center gap-1 px-2 py-1 rounded-full text-xs font-medium ' . $result->color() . ' ' . $class]) }}>
     {!! $result->icon($set) !!}
     <span>{{ $result->label() }}</span>
